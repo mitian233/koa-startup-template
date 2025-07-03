8c2d13e4e295488e3a40e48f8ea19dd3c1190ba6
mitian233 <mitian233@yahoo.co.jp>
2025-07-03 15:58:08 +0800

    Koa Router Template

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
 import Koa from 'koa';
+import Router from "koa-router";
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = new Koa();
+const router = new Router();
 
-app.use(async (ctx, next) => {
-    ctx.body = 'Hello World!';
+router.get('/', ctx => {
+    ctx.body = 'Hello Koa Router!';
 })
 
+app.use(router.routes());
+app.use(router.allowedMethods());
+
 const port = process.env.PORT || 3000;
-app.listen(port);
-console.log(`Listening on http://localhost:${port}/`);
+app.listen(port, () => {
+    console.log(`Listening on http://localhost:${port}/`);
+});
